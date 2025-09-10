d9d8c83aea603d66e0355eff991841cedeb5820a
Fucheng-Zhong <zhongfch@mail2.sysu.edu.cn>
2025-09-10 22:59:22 +0200

    modify code

--- MLCI/visualization/plot.py
+++ MLCI/visualization/plot.py
@@ -10,6 +10,7 @@ from scipy.stats import gaussian_kde
 sns.set_theme(style="ticks", palette="deep")
 from astropy.cosmology import Planck18
 import pandas as pd
+from astropy.table import Table 
 
 sim_catalog = model_new.sim_catalog
 output_para_name = model_new.output_para_name
@@ -18,6 +19,16 @@ labels = ['eFEDS', 'DR1', 'Simu']
 units = ['kpc', '$10^{12} M_{sun}$', '$10^{42} erg s^{-1}$', 'keV', '']
 colors = ['red', 'green', 'blue']
 
+# https://ui.adsabs.harvard.edu/abs/2020A%26A...641A...6P/abstract
+# Omegam = 0.315 ± 0.007
+# h0 = (67.4 ± 0.5) km s-1 Mpc-1
+# σ8 = 0.811 ± 0.006
+# Ωbh2 = 0.0224 ± 0.0001,
+h0 = (67.4/100)
+planck18_cosmology = {'Omega_medians':0.315, 'Omega_lower_errors':0.007, 'Omega_upper_errors':0.007,
+                    'Sigm8_medians':0.811, 'Sigm8_lower_errors':0.006, 'Sigm8_upper_errors':0.006,
+                    'Hubble_medians':67.4/100, 'Hubble_lower_errors':0.5/100, 'Hubble_upper_errors':0.5/100,
+                    'OmegaB_medians':0.0224/h0**2, 'OmegaB_lower_errors':0.0001/h0**2, 'OmegaB_upper_errors':0.0001/h0**2}
 
 
 def plot_distribution(dataset, labels=labels, col_rangs=col_rangs, fname=''):
@@ -75,6 +86,7 @@ def plot_confusion_matrix(results, title='test', fname='', rerange='', normalize
     plt.close()
 
 
+
 def plot_corner(results, ref_point, test_set, fname=f'./figures/test_corner.pdf', model_name='RF', show_cos_name=True, show_pdf=True, show_datapoint=True, smooth=0.5, truncation=False):
     
     prob_cols = [col for col in results.columns if col.startswith('prob_')]
@@ -94,21 +106,22 @@ def plot_corner(results, ref_point, test_set, fname=f'./figures/test_corner.pdf'
     contour_args = {'colors': 'blue', 'linestyles': 'dashed', 'linewidths': 2}
     label_names = ['$\Omega_m$', '$\sigma_8$', '$h_0$', '$\Omega_B$']
     para_range = [(0.16, 0.48), (0.45, 1.05), (0.60, 0.80), (0.03, 0.06)]
-    #para_range = [(0.16, 0.45), (0.5, 1.0), (0.65, 0.75), (0.04, 0.05)]
+    para_range = [(0.20, 0.45), (0.5, 1.0), (0.65, 0.75), (0.04, 0.05)]
     truth_kwargs = {'linewidth': 2, 'linestyle': '--'}
 
     
     if ref_point == 'None':
         truths=None
     elif ref_point == 'Planck2018':
-        # see https://arxiv.org/abs/1807.06209 Table 2
-        sigma8, h0 = 0.8102, 67.66/100
-        OmegaB = round(0.02242/h0**2,  4)
-        #planck2018 = pd.DataFrame([{'Omega':0.3111, 'Sigm8':sigma8, 'Hubble':h0, 'OmegaB':OmegaB}])
-        planck2018 = pd.DataFrame([{'Omega':Planck18.Om0, 'Sigm8':sigma8, 'Hubble':Planck18.h, 'OmegaB':Planck18.Ob0}])
+        # see https://arxiv.org/abs/1807.06209 abstract
+        sigma8, h0 = 0.8102, 67.4/100
+        OmegaB = round(0.0224/(h0**2),  4)
+        planck2018 = pd.DataFrame([{'Omega':0.315, 'Sigm8':sigma8, 'Hubble':h0, 'OmegaB':OmegaB}])
+        #planck2018 = pd.DataFrame([{'Omega':Planck18.Om0, 'Sigm8':sigma8, 'Hubble':Planck18.h, 'OmegaB':Planck18.Ob0}])
         ref_para = planck2018[output_labels]
         print(ref_para)
         truths=ref_para.values[0]
+        truths=None
     else:
         ref_para = sim_catalog[sim_catalog['name']==ref_point][output_labels]
         print(ref_para)
@@ -156,6 +169,25 @@ def plot_corner(results, ref_point, test_set, fname=f'./figures/test_corner.pdf'
             for j, label in enumerate(labels):
                 axs[idx].text(x[j], y[j], label, fontsize=label_size, ha='right', va='top', color='blue',alpha=1.0)
     
+    if ref_point == "Planck2018":
+        for i, idx in enumerate(ax_indx):
+            x = planck18_cosmology[f'{var_name[i][0]}_medians'], 
+            y = planck18_cosmology[f'{var_name[i][1]}_medians']
+            xerr = [[planck18_cosmology[f'{var_name[i][0]}_lower_errors']], [planck18_cosmology[f'{var_name[i][0]}_upper_errors']]]
+            yerr = [[planck18_cosmology[f'{var_name[i][1]}_lower_errors']], [planck18_cosmology[f'{var_name[i][1]}_upper_errors']]]
+            axs[idx].errorbar(x, y, xerr=xerr, yerr=yerr, fmt="o", markersize=0, color="red", capsize=3)
+        
+        col_names = planck2018.keys()
+        for i in range(len(col_names)):
+            x = planck18_cosmology[f'{col_names[i]}_medians']
+            xlower = x-planck18_cosmology[f'{col_names[i]}_lower_errors']
+            xupper = x+planck18_cosmology[f'{col_names[i]}_upper_errors']
+            axs[i,i].axvline(x=xlower, color='red', linestyle='--', linewidth=1)
+            axs[i,i].axvline(x=xupper, color='red', linestyle='--', linewidth=1)
+            y_min, y_max = axs[i,i].get_ylim()
+            axs[i,i].fill_betweenx([y_min, y_max], xlower, xupper, color='red', alpha=0.2, label='Planck2018, 1$\sigma$')
+
+    
     if show_pdf:
         # add the probabilities pannel
         yshift = 0.2
@@ -171,6 +203,7 @@ def plot_corner(results, ref_point, test_set, fname=f'./figures/test_corner.pdf'
             mean_prob.append(np.sum(prob[f'prob_{label}']/len(prob[f'prob_{label}'])))
         mean_prob = pd.DataFrame(data=[mean_prob], index=['mean_P'], columns=simulation_names)
         sns.heatmap(mean_prob, annot=True, fmt='.2f', cmap='Blues', cbar=False, ax=ax_bottom)
+        plt.legend()
         plt.title('mean PDF')
 
     plt.savefig(fname, bbox_inches='tight')
@@ -287,7 +320,6 @@ def plot_detection_prob(observed_data, fname=''):
     plt.savefig(fname)
 
 
-
 def plot_cosmology_paras_vs_z(bins_cosmology, aveg_cosmology, fname='./figures/test_Cosmological_Parameters_vs_z.pdf'):
     para_names = ['Omega', 'Sigm8', 'Hubble', 'OmegaB']
     label_names = ['$\Omega_m$', '$\sigma_8$', '$h_0$', '$\Omega_B$']
@@ -309,3 +341,39 @@ def plot_cosmology_paras_vs_z(bins_cosmology, aveg_cosmology, fname='./figures/t
     plt.legend(title='Parameters')
     plt.tight_layout()
     plt.savefig(fname)
+
+
+def plot_cosmology_paras_vs_test(model_names, show_name=[], fname='./figures/paras_vs_test.pdf', z_0=0.1, z_1=0.8):
+    cosmology_paras = []
+    for model_name in model_names:
+        results = pd.read_csv(f"./results/{model_name}/observation.csv")
+        subset = results[(results['z'] >= z_0) & (results['z'] < z_1)]
+        paras = get_expectation_1sigam(subset, model_name, 'observation')
+        cosmology_paras.append(paras)
+    ref_cosmology = planck18_cosmology
+    
+    cosmology_paras = Table(cosmology_paras)
+
+    para_names = ['Omega', 'Sigm8', 'Hubble', 'OmegaB']
+    label_names = ['$\Omega_m$', '$\sigma_8$', '$h_0$', '$\Omega_B$']
+
+    fig, axes = plt.subplots(nrows=1, ncols=4, sharey=True, figsize=(12, 8), gridspec_kw={'wspace': 0})
+    for para_name, label, ax in zip(para_names, label_names, axes):
+        test_num = np.arange(len(cosmology_paras))
+        value = cosmology_paras[f'{para_name}_medians']
+        value_err = np.array([cosmology_paras[f'{para_name}_lower_errors'], cosmology_paras[f'{para_name}_upper_errors']])
+        ref_value = ref_cosmology[f'{para_name}_medians']
+        ref_low_error = ref_cosmology[f'{para_name}_lower_errors']
+        ref_up_error = ref_cosmology[f'{para_name}_upper_errors']
+        ax.errorbar(value, test_num, xerr=value_err, fmt='o', capsize=4)
+        h0, err = ref_value, 0.003
+        ax.fill_betweenx([-0.1, len(cosmology_paras)-0.1], ref_value - ref_low_error, ref_value + ref_up_error, color='orange', alpha=0.4, label='Planck2018, 1$\sigma$')
+        ax.set_xlabel(label)
+    
+    if len(show_name) == 0:
+        show_name = model_names
+    axes[0].legend(loc='best')
+    plt.yticks(ticks=np.arange(len(cosmology_paras)), labels=show_name)
+    plt.tight_layout()
+    plt.savefig(fname)
+    return cosmology_paras
